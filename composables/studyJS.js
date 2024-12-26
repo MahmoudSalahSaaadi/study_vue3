9bf2c058777b324fed7e2d426c07b31b9baae5e2
Mahmoud Salah Saadi <Mahmoud.Saadi@andalusiagroup.net>
2024-12-26 12:40:44 +0200

    last change in js

--- composables/studyJS.js
+++ composables/studyJS.js
@@ -38,4 +38,135 @@ let card = `
 `;
 
 // Print
-document.write(card.repeat(4))
+
+/*
+  Arithmetic Operators
+  + Addition
+  - Subtraction
+  * Multiplication
+  / Division
+  ** Exponentiation (ES7)
+  % Modulus (Division Remainder)
+  ++ Increment [ Post / Pre ]
+  -- Decrement [ Post / Pre ]
+*/
+
+console.log(10 + 20); //
+console.log(10 + "Osama");
+
+console.log(10 - 20);
+console.log(10 - "Osama"); // NaN
+console.log(typeof NaN);
+
+console.log(10 * 20);
+console.log(10 * -20);
+
+console.log(20 / 5);
+console.log(20 / 3);
+
+console.log(2 ** 4);
+console.log(2 * 2 * 2 * 2);
+
+console.log(10 % 2);
+console.log(11 % 2); // Remove 1
+
+/*
+  Type Coercion (Type Casting)
+  - +
+  - -
+  - "" - 2
+  - false - true
+*/
+
+let a = "100";
+let b = 20;
+let c = true;
+
+console.log(+a + b + c);
+
+/*
+  String Methods
+  - Access With Index //get the char by index
+  - Access With charAt() //get the char by index
+  - length  //check the length
+  - trim() //remove spaces
+  - toUpperCase() //to Upper Case
+  - toLowerCase() //to Lower Case
+  - Chain Methods //to Upper Case
+*/
+
+let theName = "  Ahmed  ";
+
+console.log(theName);
+console.log(theName[1]);
+console.log(theName[5]);
+
+console.log(theName.charAt(1));
+console.log(theName.charAt(5));
+
+console.log(theName.length);
+console.log(theName.trim());
+
+console.log(theName.toUpperCase());
+console.log(theName.toLowerCase());
+
+console.log(theName.trim().charAt(2).toUpperCase());
+
+/*
+  String Methods
+  - indexOf(Value [Mand], Start [Opt] 0) //search the index
+  - lastIndexOf(Value [Mand], Start [Opt] Length) //last index 
+  - slice(Start [Mand], End [Opt] Not Include End) //cut the string
+  - repeat(Times) [ES6] //repeat the string
+  - split(Separator [Opt], Limit [Opt]) //split the string
+*/
+
+let aa = "Elzero Web School";
+
+console.log(aa.indexOf("Web"));
+console.log(aa.indexOf("Web", 8));
+console.log(aa.indexOf("o")); // 5
+console.log(aa.lastIndexOf("o")); // 15
+
+console.log(aa.slice(2, 6));
+console.log(aa.slice(-5, -3));
+
+console.log(aa.repeat(5));
+
+console.log(aa.split("", 6));
+
+/*
+  String Methods
+  - substring(Start [Mand], End [Opt] Not Including End) //
+  --- Start > End Will Swap 
+  --- Start < 0 It Start From 0
+  --- Use Length To Get Last Character
+  - substr(Start [Mand], Characters To Extract)     
+    --- Start >= Length = ""
+  --- Negative Start From End
+  - includes(Value [Mand], Start [Opt] Default 0) [ES6]  // return true or false
+  - startsWith(Value [Mand], Start [Opt] Default 0) [ES6] // return true or false
+  - endsWith(Value [Mand], Length [Opt] Default Full Length) [ES6] // return true or false
+*/
+
+let aad = "Elzero Web School";
+
+console.log(aad.length);
+
+console.log(aad.substring(6, 2));
+console.log(aad.substring(-10, 6)); // 0 - 6
+console.log(aad.substring(a.length - 5, a.length - 3));
+
+console.log(aad.substr(0, 6));
+console.log(aad.substr(17));
+console.log(aad.substr(-3));
+console.log(aad.substr(-5, 2));
+
+console.log(aad.includes("Web"));
+console.log(aad.includes("Web", 8));
+
+console.log(aad.startsWith("E"));
+console.log(aad.startsWith("E", 2));
+console.log(aad.startsWith("zero", 2));
+
+console.log(aad.endsWith("l"));
